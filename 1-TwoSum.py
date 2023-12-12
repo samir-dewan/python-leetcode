b996522f46f899394fdbbe6883c42390c955494b
Samir Dewan <samirdewan1995@gmail.com>
2023-12-12 21:50:55 +0000

    first leet add

---
+++ 1-TwoSum.py
@@ -0,0 +1,21 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        #we are looking for an answer where if nums[a] + nums[b] == target, return [a, b].
+
+#original answer, with no help.
+        for i, iVal in enumerate(nums):
+                for j, jVal in enumerate(nums):
+                    if iVal + jVal == target and j != i:
+                        return [i, j]
+
+#hashmap answer, with youtube help.
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashmap = {}
+        for i, j in enumerate(nums):
+            diff = target - j
+            if diff in hashmap:
+                return [hashmap[diff], i]
+            hashmap[j] = i
+            #if target - j is in the hashmap, then return [hashmapKey, i]
+            #else add an entry [i, j] into the hashmap
